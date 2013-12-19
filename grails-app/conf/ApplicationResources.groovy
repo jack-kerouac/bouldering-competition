da9cd1766cd492f58f6a03e569f3e7fbb13a1b69
Florian Rampp <Florian.Rampp@gmail.com>
2013-12-19 11:25:33 +0100

    added Foundation 5.0.2

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,5 +1,7 @@
 modules = {
     application {
-        resource url:'js/application.js'
+        resource url: 'js/vendor/jquery.js'
+        resource url: 'js/foundation.min.js'
+        resource url: 'js/application.js'
     }
 }
