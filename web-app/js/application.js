90fbf12a4765d8d7ad0c16ef2ae01a2145ff1767
Florian Rampp <Florian.Rampp@gmail.com>
2014-01-09 09:22:35 +0100

    formatting

--- web-app/js/application.js
+++ web-app/js/application.js
@@ -1,26 +1,26 @@
 if (typeof jQuery !== 'undefined') {
-	(function($) {
-		$('#spinner').ajaxStart(function() {
+	(function ($) {
+		$('#spinner').ajaxStart(function () {
 			$(this).fadeIn();
-		}).ajaxStop(function() {
-			$(this).fadeOut();
-		});
+		}).ajaxStop(function () {
+				$(this).fadeOut();
+			});
 	})(jQuery);
 }
 
 
 $(document).foundation();
 
-$(function() {
+$(function () {
 	var attr = "readonly"
 
-	if($("#top").is(":checked"))
+	if ($("#top").is(":checked"))
 		$("#tries").removeAttr(attr);
 	else
 		$("#tries").attr(attr, true);
 
-	$("#flash, #top").change(function() {
-		if($("#top").is(":checked")){
+	$("#flash, #top").change(function () {
+		if ($("#top").is(":checked")) {
 			$("#tries").removeAttr(attr);
 			$("#tries").focus();
 		}
