e275e4946db1331629a50f81876aa25120534193
Florian Rampp <Florian.Rampp@gmail.com>
2014-01-10 11:19:16 +0100

    fixed brown and purple colors

--- src/java/bcomp/gym/BoulderColor.groovy
+++ src/java/bcomp/gym/BoulderColor.groovy
@@ -5,8 +5,8 @@ import java.awt.*
 public enum BoulderColor {
 
     GREEN(Color.green), YELLOW(Color.yellow), RED(Color.red), BLUE(Color.blue), BLACK(Color.black), GRAY(Color.gray),
-    WHITE(Color.white), PINK(Color.pink), ORANGE(Color.orange), BROWN(new Color(54, 138, 57)), PURPLE(new Color(83,
-            134, 0)), BLUE_BLACK(Color.blue, Color.black), YELLOW_BLACK(Color.yellow, Color.black);
+    WHITE(Color.white), PINK(Color.pink), ORANGE(Color.orange), BROWN(new Color(138, 69, 0)), PURPLE(new Color(130,
+            0, 237)), BLUE_BLACK(Color.blue, Color.black), YELLOW_BLACK(Color.yellow, Color.black);
 
     public final Color primaryColor
     public final Color secondaryColor
