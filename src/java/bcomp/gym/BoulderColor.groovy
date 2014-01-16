985cc1363517036f19ac10cbe10614d42d5b0f20
Florian Rampp <Florian.Rampp@gmail.com>
2014-01-16 14:54:37 +0100

    adjusted color values for boulders

--- src/java/bcomp/gym/BoulderColor.groovy
+++ src/java/bcomp/gym/BoulderColor.groovy
@@ -4,25 +4,30 @@ import java.awt.*
 
 public enum BoulderColor {
 
-    GREEN(Color.green), YELLOW(Color.yellow), RED(Color.red), BLUE(Color.blue), BLACK(Color.black), GRAY(Color.gray),
-    WHITE(Color.white), PINK(Color.pink), ORANGE(Color.orange), BROWN(new Color(138, 69, 0)), PURPLE(new Color(130,
-            0, 237)), BLUE_BLACK(Color.blue, Color.black), YELLOW_BLACK(Color.yellow, Color.black);
+    GREEN("13A300"), YELLOW("DBE300"), RED("D90000"), BLUE("3D3DFF"), BLACK("0A0A0A"), GRAY("696969"),
+    WHITE("FCFCFC"), PINK("FF42F9"), ORANGE("E87400"), BROWN("8A4500"), PURPLE("8200ED"), BLUE_BLACK("0068DE",
+            "0A0A0A"), YELLOW_BLACK("DBE300", "0A0A0A");
 
     public final Color primaryColor
     public final Color secondaryColor
 
-    BoulderColor(Color primaryColor, Color secondaryColor) {
-        this.primaryColor = primaryColor
-        this.secondaryColor = secondaryColor
+    BoulderColor(String primaryColor, String secondaryColor) {
+        this.primaryColor = c(primaryColor)
+        this.secondaryColor = c(secondaryColor)
     }
 
-    BoulderColor(Color primaryColor) {
-        this.primaryColor = primaryColor
+    BoulderColor(String primaryColor) {
+        this.primaryColor = c(primaryColor)
         this.secondaryColor = null
     }
 
     public boolean hasSecondaryColor() {
         return secondaryColor != null;
+    }
+
+    private static Color c(String hex) {
+        return new Color(Integer.parseInt(hex.substring(0, 2), 16), Integer.parseInt(hex.substring(2, 4), 16),
+                Integer.parseInt(hex.substring(4, 6), 16))
     }
 
 }
