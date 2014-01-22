b49b305798a26b6e4a718cd90418c03dfb4a3f66
Florian Rampp <Florian.Rampp@gmail.com>
2014-01-22 17:38:11 +0100

    restrict ascents taken into account for user grade calculation
    only use ascents where
    - user did not flash boulder easier than his ability
    - user did flash boulder harder than his ability

--- grails-app/services/bcomp/GradeLearningService.groovy
+++ grails-app/services/bcomp/GradeLearningService.groovy
@@ -1,5 +1,6 @@
 package bcomp
 
+import bcomp.Ascent.Style
 import bcomp.aaa.User
 import bcomp.gym.Boulder
 import bcomp.gym.Grade
@@ -52,19 +53,19 @@ class GradeLearningService {
         }
     }
 
+    // probability should be 90% that personal level is at most two font grades away from personal level
+    // => sigma = 1.645 (see http://de.wikipedia.org/wiki/Normalverteilung)
+    public final static double VARIANCE_OF_INITIAL_GRADE = (2 * Grade.oneFontGradeDifference() / 1.645)**2
 
     TentativeGrade calculateInitialGrade(User user) {
-        // probability should be 90% that personal level is at most two font grades away from personal level
-        // => sigma = 1.645 (see http://de.wikipedia.org/wiki/Normalverteilung)
-        double variance = (2 * Grade.oneFontGradeDifference() / 1.645)**2
-
-        return new TentativeGrade(mean: user.initialGrade, variance: variance)
+        return new TentativeGrade(mean: user.initialGrade, variance: VARIANCE_OF_INITIAL_GRADE)
     }
 
     /**
      * Calculate the grade for the given user if only the given ascents are considered for grade calculation.
      * The initial grade of the user is taken into account as well.
      */
+    // TODO: pass current user grade as parameter?
     TentativeGrade calculateGrade(User user, def ascentsToConsider) {
         // if there is only one ascent, it counts as 1 : numberOfAscentsThatInitialGradeCountsFor in the grade
         // calculation
@@ -74,12 +75,25 @@ class GradeLearningService {
         double weights = 0
         ascentsToConsider.each { Ascent ascent ->
             Boulder boulder = ascent.boulder
-            def weight = 1 / boulder.grade.variance
-            gradeValue += boulder.grade.value * weight
-            weights += weight
+
+            // only consider this ascent if user did not flash easy boulder or user flashed harder boulder
+            // TODO: use variance here as well? boulder with high variance might count as well though it is
+            // harder/easier
+            def didNotFlashEasyBoulder = ascent.boulder.grade.mean < user.grade.mean && ascent.style != Style.flash
+            def didFlashHardBoulder = ascent.boulder.grade.mean > user.grade.mean && ascent.style == Style.flash
+            if (didNotFlashEasyBoulder || didFlashHardBoulder) {
+                def weight = 1 / boulder.grade.variance
+                gradeValue += boulder.grade.value * weight
+                weights += weight
+            }
+            // count current user grade otherwise
+            else {
+                def weight = 1 / user.grade.variance
+                gradeValue += user.grade.value * weight
+                weights += weight
+            }
         }
-        // TODO: what is the weight for the initial grade?
-        def weight = 1 / user.grade.variance
+        def weight = 1 / VARIANCE_OF_INITIAL_GRADE
         gradeValue += numberOfAscentsThatInitialGradeCountsFor * user.initialGrade.value * weight
         weights += numberOfAscentsThatInitialGradeCountsFor * weight
 
@@ -116,7 +130,6 @@ class GradeLearningService {
         }
         return new TentativeGrade(mean: grade, variance: variance)
     }
-
 
 
     TentativeGrade calculateGrade(Boulder boulder, def ascentsToConsider) {
