50cc5415777837f24e0c9a7fc76ae3d429678415
Florian Rampp <Florian.Rampp@gmail.com>
2014-01-26 14:07:56 +0100

    removed jquery panzoom, not needed anymore

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -1,7 +1,6 @@
 modules = {
     application {
         resource url: 'js/vendor/jquery.js'
-        resource url: 'js/vendor/jquery.panzoom.js'
         resource url: 'js/foundation.min.js'
         resource url: 'js/vendor/jquery.flot.js'
         resource url: 'js/vendor/jquery.flot.time.js'
