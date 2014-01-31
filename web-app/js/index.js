d9ed0b751149687e30f35202f117b8ef5eaa0167
Florian Rampp <Florian.Rampp@gmail.com>
2014-01-31 12:41:31 +0100

    start page has proper content now

--- web-app/js/index.js
+++ web-app/js/index.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('#intro').parallax("50%", 0.3);
-    $('#how-it-works').parallax("50%", 0.5);
-    $('#get-the-app').parallax("50%", 0.3);
+    $('.intro > div:nth-child(1)').parallax("50%", 0.3);
+    $('.intro > div:nth-child(2)').parallax("50%", 0.5);
+    $('.intro > div:nth-child(3)').parallax("50%", 0.3);
 })
