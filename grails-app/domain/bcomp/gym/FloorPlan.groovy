a247ccde3bc3cdc36f9db688a4b852cbeb7dd423
Florian Rampp <Florian.Rampp@gmail.com>
2014-02-15 18:26:59 +0100

    removed old mapping and constraint from floor plan
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   grails-app/controllers/bcomp/api/GymController.groovy
    #	modified:   grails-app/domain/bcomp/gym/Gym.groovy
    #	modified:   src/groovy/bcomp/SampleData.groovy
    #

--- grails-app/domain/bcomp/gym/FloorPlan.groovy
+++ grails-app/domain/bcomp/gym/FloorPlan.groovy
@@ -1,14 +1,6 @@
 package bcomp.gym
 
-import javax.imageio.ImageIO
-import javax.imageio.stream.ImageInputStream
-import java.awt.Image
-
 class FloorPlan {
-
-    static transients = ['image']
-
-    static mapping = { imageAsPng sqlType: 'mediumblob'}
 
     static belongsTo = [gym: Gym]
 
