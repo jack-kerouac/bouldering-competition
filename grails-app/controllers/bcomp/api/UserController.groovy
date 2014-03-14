ad9a291e41ce25f3a76f5a27daa7e86265d428a2
Florian Rampp <Florian.Rampp@gmail.com>
2014-03-14 13:53:41 +0100

    lowercasing email address when login or registering

--- grails-app/controllers/bcomp/api/UserController.groovy
+++ grails-app/controllers/bcomp/api/UserController.groovy
@@ -42,6 +42,7 @@ class UserController extends RestfulController {
 
     // based on http://raibledesigns.com/rd/entry/implementing_ajax_authentication_using_jquery
     def loginOrRegister(Credentials c) {
+        c.email = c.email.toLowerCase()
         User user = User.findByUsername(c.email);
         if(user != null) {
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(c.email, c.password);
